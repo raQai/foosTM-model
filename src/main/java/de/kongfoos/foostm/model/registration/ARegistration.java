9d2a3550544e715cce164bc00173fd0b9dc2ea27
raQai <patrick.bgxn@gmail.com>
2016-07-28 20:31:43 +0200

    (ARegistration) implement setters with basic functionality

--- src/main/java/de/kongfoos/foostm/model/registration/ARegistration.java
+++ src/main/java/de/kongfoos/foostm/model/registration/ARegistration.java
@@ -3,5 +3,13 @@ package de.kongfoos.foostm.model.registration;
 import de.kongfoos.foostm.model.discipline.ADiscipline;
 import de.kongfoos.foostm.model.team.ATeam;
 
+import javax.validation.constraints.NotNull;
+import java.util.Collection;
+
 public abstract class ARegistration<T extends ATeam, D extends ADiscipline> implements IRegistration<T, D> {
+    @Override
+    public void setDisciplines(@NotNull Collection<D> disciplines) {
+        getDisciplines().forEach(this::removeDiscipline);
+        disciplines.forEach(this::addDiscipline);
+    }
 }
