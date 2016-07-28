52e8de2b4f9663071763e72d64c776013c6c6d98
raQai <patrick.bgxn@gmail.com>
2016-07-28 18:37:00 +0200

    (ITournament) Add setters for lists and additional helper methods for the model

--- src/main/java/de/kongfoos/foostm/model/tournament/ITournament.java
+++ src/main/java/de/kongfoos/foostm/model/tournament/ITournament.java
@@ -4,17 +4,26 @@ import de.kongfoos.foostm.model.discipline.DisciplineImpl;
 import de.kongfoos.foostm.model.registration.RegistrationImpl;
 
 import javax.validation.constraints.NotNull;
+import java.util.Collection;
 import java.util.List;
 
 interface ITournament<D extends DisciplineImpl, R extends RegistrationImpl> {
 
     List<D> getDisciplines();
 
+    void setDisciplines(@NotNull Collection<D> disciplines);
+
+    void clearDisciplines();
+
     boolean addDiscipline(@NotNull D discipline);
 
     boolean removeDiscipline(@NotNull D discipline);
 
     List<R> getRegistrations();
+
+    void setRegistrations(@NotNull Collection<R> registrations);
+
+    void clearRegistrations();
 
     boolean addRegistration(@NotNull R registration);
 
