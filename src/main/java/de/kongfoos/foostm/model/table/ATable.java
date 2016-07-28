88c9fcb601afed94e22d266a94c837278211b4e5
raQai <patrick.bgxn@gmail.com>
2016-07-28 20:37:15 +0200

    Reformat code

--- src/main/java/de/kongfoos/foostm/model/table/ATable.java
+++ src/main/java/de/kongfoos/foostm/model/table/ATable.java
@@ -3,6 +3,7 @@ package de.kongfoos.foostm.model.table;
 import de.kongfoos.foostm.model.match.AMatch;
 
 public abstract class ATable<M extends AMatch> implements ITable<M> {
+
     @Override
     public void removeMatch() {
         setMatch(null);
