e37e19b7b64b1b9c7315fe967ad7db5ef0272c34
raQai <patrick.bgxn@gmail.com>
2016-07-28 19:09:09 +0200

    (ADiscipline) implement setters with basic functionality

--- src/main/java/de/kongfoos/foostm/model/discipline/ADiscipline.java
+++ src/main/java/de/kongfoos/foostm/model/discipline/ADiscipline.java
@@ -5,12 +5,30 @@ import de.kongfoos.foostm.model.table.ATable;
 import de.kongfoos.foostm.model.team.ATeam;
 
 import javax.validation.constraints.NotNull;
+import java.util.Collection;
 import java.util.List;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 public abstract class ADiscipline<T extends ATeam, M extends AMatch<T>, P extends ATable<M>>
         implements IDiscipline<T, M, P> {
+
+    @Override
+    public void setPatricipationRules(@NotNull Collection<Predicate<T>> predicates) {
+        getParticipationRules().forEach(this::removeRule);
+        predicates.forEach(this::addRule);
+    }
+
+    @Override
+    public boolean allowsParticipation(@NotNull T team) {
+        return getParticipationRules().stream().allMatch(p -> p.test(team));
+    }
+
+    @Override
+    public void setTeams(@NotNull Collection<T> teams) {
+        getTeams().forEach(this::removeTeam);
+        teams.forEach(this::addTeam);
+    }
 
     @Override
     public List<T> getTeams(Predicate<T> predicate) {
@@ -22,18 +40,25 @@ public abstract class ADiscipline<T extends ATeam, M extends AMatch<T>, P extend
     }
 
     @Override
+    public void setMatches(@NotNull Collection<M> matches) {
+        getMatches().forEach(this::removeMatch);
+        matches.forEach(this::addMatch);
+    }
+
+    @Override
     public List<M> getOpenMatches() {
         return getMatches().stream().filter(AMatch::isOpen).collect(Collectors.toList());
     }
 
     @Override
-    public List<P> getFreeTables() {
-        return getTables().stream().filter(ATable::isFree).collect(Collectors.toList());
+    public void setTables(@NotNull Collection<P> tables) {
+        getTables().forEach(this::removeTable);
+        tables.forEach(this::addTable);
     }
 
     @Override
-    public boolean allowsParticipation(@NotNull T team) {
-        return getParticipationRules().stream().allMatch(p -> p.test(team));
+    public List<P> getFreeTables() {
+        return getTables().stream().filter(ATable::isFree).collect(Collectors.toList());
     }
 
     @Override
