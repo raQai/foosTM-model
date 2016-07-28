c92267049d11e23ef6fb6698e7de78de1877337d
raQai <patrick.bgxn@gmail.com>
2016-07-28 20:35:28 +0200

    (ATeam) implement setters with basic functionality

--- src/main/java/de/kongfoos/foostm/model/team/ATeam.java
+++ src/main/java/de/kongfoos/foostm/model/team/ATeam.java
@@ -2,7 +2,17 @@ package de.kongfoos.foostm.model.team;
 
 import de.kongfoos.foostm.model.player.APlayer;
 
+import javax.validation.constraints.NotNull;
+import java.util.Collection;
+
 public abstract class ATeam<P extends APlayer> implements ITeam<P>{
+
+    @Override
+    public void setPlayers(@NotNull Collection<P> players) {
+        getPlayers().forEach(this::removePlayer);
+        players.forEach(this::addPlayer);
+    }
+
     @Override
     public boolean isSingles() {
         return getType().equals(Type.SINGLES);
