b82ab5bbc19f24a283c95b69d338026269cb039a
raQai <patrick.bgxn@gmail.com>
2016-07-28 19:40:28 +0200

    (APlayer) Add basic implementation of helper methods #isSenior and #isJunior

--- src/main/java/de/kongfoos/foostm/model/player/APlayer.java
+++ src/main/java/de/kongfoos/foostm/model/player/APlayer.java
@@ -1,6 +1,11 @@
 package de.kongfoos.foostm.model.player;
 
+import java.util.Calendar;
+
 public abstract class APlayer implements IPlayer {
+
+    private static final int SENIOR_MINIMUM_AGE = 51;
+    private static final int JUNIOR_MAXIMUM_AGE = 18;
 
     @Override
     public String getName() {
@@ -15,6 +20,16 @@ public abstract class APlayer implements IPlayer {
     @Override
     public boolean isFemale() {
         return getGender().equals(Gender.FEMALE);
+    }
+
+    @Override
+    public boolean isSenior() {
+        return (Calendar.getInstance().get(Calendar.YEAR) - getBirthDate().get(Calendar.YEAR)) >= SENIOR_MINIMUM_AGE;
+    }
+
+    @Override
+    public boolean isJunior() {
+        return (Calendar.getInstance().get(Calendar.YEAR) - getBirthDate().get(Calendar.YEAR)) <= JUNIOR_MAXIMUM_AGE;
     }
 
     @Override
