77fdb65271e5cd6f77c3311503d000b38a65a205
raQai <patrick.bgxn@gmail.com>
2016-07-28 18:35:23 +0200

    (ITeam) Add setters for lists and additional helper methods for the model

--- src/main/java/de/kongfoos/foostm/model/team/ITeam.java
+++ src/main/java/de/kongfoos/foostm/model/team/ITeam.java
@@ -3,11 +3,16 @@ package de.kongfoos.foostm.model.team;
 import de.kongfoos.foostm.model.player.PlayerImpl;
 
 import javax.validation.constraints.NotNull;
+import java.util.Collection;
 import java.util.List;
 
 interface ITeam<P extends PlayerImpl> {
 
     List<P> getPlayers();
+
+    void setPlayers(@NotNull Collection<P> players);
+
+    void clearPlayers();
 
     void addPlayer(@NotNull P player);
 
