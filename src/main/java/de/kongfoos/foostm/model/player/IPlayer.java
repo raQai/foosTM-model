c982d2061bfdddc79fb58d31a4d0517617fd4416
raQai <patrick.bgxn@gmail.com>
2016-07-28 18:29:56 +0200

    (IPlayer) Remove db annotations because they dont belong to this branch and current state

--- src/main/java/de/kongfoos/foostm/model/player/IPlayer.java
+++ src/main/java/de/kongfoos/foostm/model/player/IPlayer.java
@@ -2,57 +2,36 @@ package de.kongfoos.foostm.model.player;
 
 import java.util.Date;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
-import javax.persistence.Id;
-import javax.persistence.Table;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
-import javax.persistence.Transient;
-
-@Entity(name="player")
-@Table(name="player")
 interface IPlayer {
 
-	@Column(name="forename")
     String getForename();
 
     void setForename(String forename);
 
-    @Column(name="surname")
     String getSurname();
 
     void setSurname(String surname);
 
-    @Enumerated(EnumType.STRING)
     Gender getGender();
 
     void setGender(Gender gender);
 
-    @Temporal(TemporalType.DATE)
     Date getBirthDate();
 
     void setBirthDate(Date birthDate);
 
-    @Id
-    @Column(name="itsf")
     String getItsf();
 
     void setItsf(String itsf);
 
-    @Column(name="dtfb")
     String getDtfb();
 
     void setDtfb(String dtfb);
 
-    @Column(name="club")
     String getClub();
 
     void setClub(String club);
 
-    @Transient
     String getName();
 
     boolean isMale();
